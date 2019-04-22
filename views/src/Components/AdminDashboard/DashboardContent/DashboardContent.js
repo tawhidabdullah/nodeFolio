df4a479cc68c4ac44e542e11ea6e5522ddd7a4ff
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-23 04:17:35 +0600

    administrating..

--- views/src/Components/AdminDashboard/DashboardContent/DashboardContent.js
+++ views/src/Components/AdminDashboard/DashboardContent/DashboardContent.js
@@ -92,29 +92,3 @@ class DashboardContent extends Component {
 }
 
 export default DashboardContent;
-
-/* 
-
-    <div>
-      <section class="main" id="banner">
-        <div class="banner">
-          
-          <div class="container banner-inner">
-            <div class="contant">
-              <p class="top">Hi</p>
-              <h2>
-                I am <span>Tawhid Abdullah</span>
-              </h2>
-              {/* <p class="bottom">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
-                suscipit accusamus in optio! Assumenda{" "}
-              </p> 
-              </div>
-              </div>
-            </div>
-          </section>
-        </div>
-
-
-
-*/
--- views/src/Components/AdminDashboard/ProductsContent/ProductsContent.js
+++ views/src/Components/AdminDashboard/ProductsContent/ProductsContent.js
@@ -38,27 +38,77 @@ class ProductsContent extends Component {
       <div>
         <div className="container">
           <div className="header-wrapper">
-            <div className="title">Welcome back, Tawhid Abdullah!</div>
-            <div className="note">
-              Recent: <span className="focus">$250 </span>to{" "}
-              <span className="focus">Best Buy</span> on Saturday, June 5.
+            <div className="title">Some recent, Projects!</div>
+            <div class="produc-wrap ">
+              <div class="search">
+                <input
+                  onChange={this.onSearchInputChange}
+                  value={this.state.searchInput}
+                  type="text"
+                  class="searchTerm searchTerm__red"
+                  placeholder="Search products by name.."
+                />
+                <button type="submit" class="searchButton searchButton__red">
+                  <i class="fa fa-search" />
+                </button>
+              </div>
             </div>
-
-            <span
-              className="material-button"
-              onClick={this.onMaterialButtonclick}
-            >
-              <i className="fa fa-plus" />
-            </span>
           </div>
           <div className="content-wrapper">
-            <div className="table-wrapper">
-              <ul className="horizontal col header">
-                <li className="content">Updated Date</li>
-                <li className="content">Payee</li>
-                <li className="content ">Description</li>
-                <li className="content right">Remaining</li>
-              </ul>
+            <div class="blog-slider">
+              <div class="blog-slider__wrp swiper-wrapper">
+                <div class="blog-slider__item swiper-slide">
+                  <div class="blog-slider__img">
+                    <img src="../../../assets/imgs/sharpstore1.PNG" alt="" />
+                  </div>
+                  <div class="blog-slider__content">
+                    <span class="blog-slider__code">26 December 2019</span>
+                    <div class="blog-slider__title">Sharp Store</div>
+                    <div class="blog-slider__text">
+                      A online store for all kind of products with easy to use
+                      dashboard{" "}
+                    </div>
+                    <a
+                      href="https://young-everglades-31692.herokuapp.com/products"
+                      class="blog-slider__button"
+                    >
+                      PREVIEW
+                    </a>
+                    <a
+                      href="https://github.com/tawhidabdullah/sharpStore"
+                      class="blog-slider__button blog-slider__button-code"
+                    >
+                      VIEW CODE
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="blog-slider">
+              <div class="blog-slider__wrp swiper-wrapper">
+                <div class="blog-slider__item swiper-slide">
+                  <div class="blog-slider__img">
+                    <img src="../../../assets/imgs/sharpstore1.PNG" alt="" />
+                  </div>
+                  <div class="blog-slider__content">
+                    <span class="blog-slider__code">26 December 2019</span>
+                    <div class="blog-slider__title">Sharp Store</div>
+                    <div class="blog-slider__text">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Recusandae voluptate repellendus magni illo ea animi?{" "}
+                    </div>
+                    <a href="#" class="blog-slider__button">
+                      PREVIEW
+                    </a>
+                    <a
+                      href="#"
+                      class="blog-slider__button blog-slider__button-code"
+                    >
+                      VIEW CODE
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -68,3 +118,18 @@ class ProductsContent extends Component {
 }
 
 export default ProductsContent;
+
+/*
+
+<div className="table-wrapper">
+              <ul className="horizontal col header">
+                <li className="content">Updated Date</li>
+                <li className="content">Payee</li>
+                <li className="content ">Description</li>
+                <li className="content right">Remaining</li>
+              </ul>
+            </div>
+
+
+
+*/
